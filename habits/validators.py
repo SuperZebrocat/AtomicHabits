e299776f03eccfa87b1f06155c090f4d372ec2e2
екатерина ермакова <katka_z_jones@mail.ru>
2025-04-29 20:46:55 +0200

    Validators for habit model created

---
+++ habits/validators.py
@@ -0,0 +1,14 @@
+from rest_framework.exceptions import ValidationError
+
+
+def validate_periodicity(periodicity):
+    """Валидация периодичности выполнения привычки"""
+    if not 0 < periodicity <= 7:
+        raise ValidationError(f'Привычку нельзя выполнять реже одного раза в неделю')
+
+
+def validate_lead_time(lead_time):
+    """Валидация времени выполнения привычки"""
+    if lead_time > 120:
+        raise ValidationError(f'Время выполнения привычки не должно превышать 2-х минут')
+
