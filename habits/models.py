bf713afc75429c099fbaf5eebfd7637120f9d1fc
екатерина ермакова <katka_z_jones@mail.ru>
2025-04-29 23:04:51 +0200

    Habit model updated

--- habits/models.py
+++ habits/models.py
@@ -10,7 +10,7 @@ class Habit(models.Model):
     action = models.CharField(max_length=255, verbose_name="Действие")
     is_pleasant = models.BooleanField(default=False, verbose_name="Приятная привычка")
     associated_habit = models.ManyToManyField("self", blank=True, null=True, verbose_name="Связанная привычка")
-    periodicity = models.PositiveIntegerField(verbose_name="Периодичность выполнения")
+    periodicity = models.PositiveIntegerField(default=1, blank=True, null=True, verbose_name="Периодичность выполнения")
     reward = models.CharField(max_length=255, null=True, blank=True, verbose_name="Вознаграждение")
     lead_time = models.PositiveIntegerField(verbose_name="Продолжительность выполнения")
     is_public = models.BooleanField(default=False, verbose_name="Публичная привычка")
@@ -21,10 +21,3 @@ class Habit(models.Model):
     class Meta:
         verbose_name = "Привычка"
         verbose_name_plural = "Привычки"
-
-
-# class HabitExecution(models.Model):
-#     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, verbose_name="Владелец")
-#     habit = models.ForeignKey(Habit, on_delete=models.CASCADE, null=True, blank=True, verbose_name="Привычка", related_name="habit")
-#     executed_at = models.DateTimeField()
-#     is_executed = models.BooleanField()
