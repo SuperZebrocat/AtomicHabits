d52f8f8e6bc38209ea8da8fa0f50c3ce65b6a1c8
екатерина ермакова <katka_z_jones@mail.ru>
2025-05-04 23:15:44 +0200

    Field for telegram chat id added in user model

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name="Email")
+    tg_chat_id = models.CharField(max_length=50, verbose_name="Телеграм chat-id")
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
