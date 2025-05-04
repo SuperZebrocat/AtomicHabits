f23a482951f9c3d32191f5907720ae85ab5918dc
екатерина ермакова <katka_z_jones@mail.ru>
2025-05-04 23:13:01 +0200

    Celery configuration updated

--- config/celery_config.py
+++ config/celery_config.py
@@ -8,7 +8,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('celery')
+app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
