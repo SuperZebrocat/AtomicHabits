75681b0844d566d0cbf05faaaf6531b062fc171c
екатерина ермакова <katka_z_jones@mail.ru>
2025-05-05 01:17:59 +0200

    Small fixes

--- habits/validators.py
+++ habits/validators.py
@@ -4,11 +4,10 @@ from rest_framework.exceptions import ValidationError
 def validate_periodicity(periodicity):
     """Валидация периодичности выполнения привычки"""
     if not 0 < periodicity <= 7:
-        raise ValidationError(f'Привычку нельзя выполнять реже одного раза в неделю')
+        raise ValidationError("Привычку нельзя выполнять реже одного раза в неделю")
 
 
 def validate_lead_time(lead_time):
     """Валидация времени выполнения привычки"""
     if lead_time > 120:
-        raise ValidationError(f'Время выполнения привычки не должно превышать 2-х минут')
-
+        raise ValidationError("Время выполнения привычки не должно превышать 2-х минут")
