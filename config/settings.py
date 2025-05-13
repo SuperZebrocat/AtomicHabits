e2d88c95b69355d9a6ce41fa325d804351de6ac4
екатерина ермакова <katka_z_jones@mail.ru>
2025-05-13 13:21:46 +0200

    Small fixes

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ load_dotenv(override=True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-1(r2(+qgbfyqunszoa_wq=nzp3vl2j!86yr!5ls4#^gzjzy8r8"
 
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
